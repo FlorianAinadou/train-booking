6152d229ec4499b14e976633f9b8b61cd54d361a
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-10-22 22:33:46 +0200

    #17 Première implémentation du service externe bank

---
+++ externes/bank/app.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const cors = require('@koa/cors');
+const bodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
+
+const app = new Koa();
+const PORT = 7000;
+
+const paidRoute  = require('./src/server/routes/paidRoute');
+
+app.use(bodyParser());
+app.use(logger());
+app.use(cors({origin: '*', exposeHeaders: '*'}));
+app.use(paidRoute.routes());
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+  });
+  
+  
+  module.exports = {
+    server
+  };
