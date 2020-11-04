87b958878fc596bd3a51fbf52716f71a23f0bf42
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-11-04 16:38:39 +0100

    #28 implémentation du tests de customers.features

--- tests/features/step_definitions/stepdefs.js
+++ tests/features/step_definitions/stepdefs.js
@@ -8,9 +8,32 @@ function isItFriday(today) {
       return "Nope";
     }
   }
+
+// const https = require('https')
+// const options = {
+//   hostname: 'whatever.com',
+//   port: 443,
+//   path: '/todos',
+//   method: 'GET'
+// }
+
+// const req = https.request(options, res => {
+//   console.log(`statusCode: ${res.statusCode}`)
+
+//   res.on('data', d => {
+//     process.stdout.write(d)
+//   })
+// })
+
+// req.on('error', error => {
+//   console.error(error)
+// })
+
+// req.end()
   
   Given('today is Sunday', function () {
     this.today = 'Sunday';
+    
   });
   
   When('I ask whether it\'s Friday yet', function () {
